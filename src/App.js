2c7ca20606af6f985a9108f4a4a7261c05352d25
Mariya <impa0601@mail.ru>
2022-01-18 01:49:37 +0300

    add lesson-1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Message from "./Message";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header
+          className={`App-header ${props.showred ? 'header-red' : 'header-blue'}`}
+          style={{top: props.topPosition || '10px' }}
+      >
+          My first React App
+          <h3>Hello, {props.name}</h3>
+          <Message text={'Задание 1'}/>
       </header>
     </div>
   );
